3eb4568702203c623a867c7a34d8fd8f1ba04407
Alexander <pchp250g8@tutamail.com>
2026-02-16 14:35:03 +0300

    Добавьте файлы проекта.

---
+++ BinToDec/Module1.vb
@@ -0,0 +1,29 @@
+﻿Imports System.Text.RegularExpressions
+
+Module Module1
+
+	Sub Main()
+		Dim nDecNum = 0
+		Dim nBinPower = 1
+		Dim nMaxBinaryLen = Math.Truncate(Math.Log(UInteger.MaxValue, 2))
+		Dim oRegEx = New Regex("[0-1]")
+		Console.WriteLine("Input a binary number")
+		Dim strLine = Console.ReadLine()
+		Dim nStrLen = strLine.Length
+		Dim nMatches = oRegEx.Matches(strLine).Count
+		Dim bRightString = (nStrLen <= nMaxBinaryLen) AndAlso (nStrLen = nMatches)
+		If Not bRightString Then
+			Console.WriteLine("Wrong binary number format!!!")
+			Console.Read()
+			Return
+		End If
+		For i As Integer = 0 To nStrLen - 1
+			Dim nBinDight As Integer = AscW(strLine.Chars(nStrLen - 1 - i)) - AscW("0"c)
+			nDecNum += (nBinDight * nBinPower)
+			nBinPower *= 2
+		Next i
+		Console.WriteLine("The decenary equivalent of the binary number {0} is {1}", strLine, nDecNum)
+		Console.Read()
+	End Sub
+
+End Module
---
+++ BinToDec/My Project/AssemblyInfo.vb
@@ -0,0 +1,32 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Общие сведения об этой сборке предоставляются следующим набором
+' набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+' связанные со сборкой.
+
+' Проверьте значения атрибутов сборки
+
+<Assembly: AssemblyTitle("BinToDec")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("BinToDec")>
+<Assembly: AssemblyCopyright("Copyright ©  2026")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+<Assembly: Guid("4711dec1-b198-4df9-b4a5-7e67607ed273")>
+
+' Сведения о версии сборки состоят из следующих четырех значений:
+'
+'      Основной номер версии
+'      Дополнительный номер версии
+'   Номер сборки
+'      Редакция
+'
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
